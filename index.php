ae74fbec6cbb9d58f12ebdb2dbfc6883b67696cc
Milos Vnucko <milosz.milosz@gmail.com>
2018-03-11 22:06:45 +0100

    implemented image resizing and code adjustments

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-require_once('src/Crawler.php');
+require_once('src/Salamander.php');
 
 // DEBUG
 error_reporting(E_ALL);
 $path = isset($_GET['path']) ? $_GET['path'] : '';
 
 
-$crawler = new Crawler($path, array(
+$crawler = new Salamander($path, array(
     'images_dir' => 'photos',
     'thumbnails_dir' => 'thumbnails',
     'cache_dir' => 'cache',
